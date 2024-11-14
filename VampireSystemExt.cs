8c325b53d9e8c146e0251210e4ae65244db5885b
joitz01 <romeov007@gmail.com>
2024-11-14 14:48:38 -0700

    Create VampireSystemExt.cs

---
+++ VampireSystemExt.cs
@@ -0,0 +1,12 @@
+using System;
+using Server;
+using System.Xml;
+using Server.Misc;
+using Server.Gumps;
+using Server.Items;
+using Server.Mobiles;
+using Server.Network;
+using Server.Regions;
+using System.Collections;
+using Server.Engines.Craft;
+using System.Collections.Generic;
